9db5be7f4fb6db0119174b471d476f197f1c4a9e
DAVIDNYARKO123 <nyarkprince6@gmail..com>
2023-02-11 22:28:17 -0500

    update script to take frame

--- yolov8_n_opencv.py
+++ yolov8_n_opencv.py
@@ -1,7 +1,8 @@
-import numpy as np
-import cv2
-from ultralytics import YOLO
 import random
+
+import cv2
+import numpy as np
+from ultralytics import YOLO
 
 # opening the file in read mode
 my_file = open("utils/coco.txt", "r")
@@ -16,15 +17,15 @@ my_file.close()
 # Generate random colors for class list
 detection_colors = []
 for i in range(len(class_list)):
-    r = random.randint(0,255)
-    g = random.randint(0,255)
-    b = random.randint(0,255)
-    detection_colors.append((b,g,r))
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    detection_colors.append((b, g, r))
 
 # load a pretrained YOLOv8n model
-model = YOLO("weights/yolov8n.pt", "v8") 
+model = YOLO("weights/yolov8n.pt", "v8")
 
-# Vals to resize video frames | small frame optimise the run 
+# Vals to resize video frames | small frame optimise the run
 frame_wid = 640
 frame_hyt = 480
 
@@ -44,14 +45,11 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
-    #  resize the frame | small frame optimise the run 
+    #  resize the frame | small frame optimise the run
     # frame = cv2.resize(frame, (frame_wid, frame_hyt))
 
-    # write frame to be loaded by the prediction module
-    cv2.imwrite("inference/images/frame.png",frame)
-
-    # Predict on image 
-    detect_params = model.predict(source="inference/images/frame.png", conf=0.45,save=False)
+    # Predict on image
+    detect_params = model.predict(source=[frame], conf=0.45, save=False)
 
     # Convert tensor array to numpy
     DP = detect_params[0].numpy()
@@ -79,10 +77,7 @@ while True:
             font = cv2.FONT_HERSHEY_COMPLEX
             cv2.putText(
                 frame,
-                class_list[int(clsID)]
-                + " "
-                + str(round(conf, 3))
-                + "%",
+                class_list[int(clsID)] + " " + str(round(conf, 3)) + "%",
                 (int(bb[0]), int(bb[1]) - 10),
                 font,
                 1,
@@ -91,10 +86,10 @@ while True:
             )
 
     # Display the resulting frame
-    cv2.imshow('ObjectDetection', frame)
+    cv2.imshow("ObjectDetection", frame)
 
     # Terminate run when "Q" pressed
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(1) == ord("q"):
         break
 
 # When everything done, release the capture
